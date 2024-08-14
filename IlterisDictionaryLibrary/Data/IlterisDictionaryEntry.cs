020aa361c8621d91450b0a89ee46e54745e64dab
DQRC <rydanchb@gmail.com>
2024-08-15 00:57:13 +0200

    enabled showing and hiding of certain entries. still getting the gist of html-c# interactions

---
+++ IlterisDictionaryLibrary/Data/IlterisDictionaryEntry.cs
@@ -0,0 +1,23 @@
+﻿namespace IlterisDictionaryLibrary.Data
+{
+    public class IlterisDictionaryEntry
+    {
+        public Guid EntryId { get; } = Guid.NewGuid();
+
+        public string ProtoTurkicRoot { get; init; }
+
+        public string TurkishVariant { get; init; }
+
+        public string Loanword { get; init; }
+
+        public string Meaning { get; init; }
+
+        public string FurtherReading { get; init; }
+
+        public string OtherVariants { get; init; }
+
+        public string RelatedTo { get; init; }
+
+        public bool MeaningVisible { get; set; }
+    }
+}
