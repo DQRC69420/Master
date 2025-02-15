779229e84278d26b041a70a841e12bbffea53cf9
DQRC <rydanchb@gmail.com>
2025-02-15 01:37:50 +0100

    Revert "added maui host project"
    This reverts commit 37e7ec82a3e3b0c84bbce6da597ad476f4dd926a.

--- MAUIBlazorHost/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using MAUIBlazorHost.Data;
-global using MAUIBlazorHost.PageModels;
-global using MAUIBlazorHost.Pages;
-global using MAUIBlazorHost.Services;
-global using MAUIBlazorHost.Utilities;
