0a2d5aa9909a4f54446afa11331a7bc047830a93
DQRC <rydanchb@gmail.com>
2025-02-27 01:35:34 +0100

    edited first line of .csproj of Maui project to replace "Microsoft.NET.Sdk" with "Microsoft.NET.Sdk.Razor"

--- MAUIBlazorHost/MauiProgram.cs
+++ MAUIBlazorHost/MauiProgram.cs
@@ -1,25 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.WebView.Maui;
 
 namespace MAUIBlazorHost
 {
     public static class MauiProgram
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder(); 
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddMauiBlazorWebView();
+			builder.Services.AddBlazorWebViewDeveloperTools();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+			builder.Logging.AddDebug();
+			builder.Logging.SetMinimumLevel(LogLevel.Debug);
 #endif
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
